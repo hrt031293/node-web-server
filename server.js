da3757b70408664bee26cfff02428ad1217d90da
hrt031293 <hrt1993@gmail.com>
2017-12-27 00:39:39 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const express=require('express');
+const hbs=require('hbs');
+const fs=require('fs');
+
+var app= express();
+
+hbs.registerPartials(__dirname+'/views/partials')
+
+app.set('view engine','hbs');
+
+
+
+app.use((req,res,next)=>{
+    var now= new Date().toString();
+    var log=`${now}: ${req.method}  ${req.url}`
+    console.log(log);
+    fs.appendFileSync('server.log', log +'\n');
+    
+    next();
+})
+
+// app.use((req,res,next)=>{
+//     res.render('maintainance.hbs');
+// });
+
+hbs.registerHelper('getCurrentYear', ()=>{
+return new Date().getFullYear();
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('screamIt',(text)=>{
+return text.toUpperCase();
+});
+app.get('/',(req,res)=>{
+//res.send("<h1>Hello Express!</h1>");
+    res.send({
+        name: 'Harshit',
+        likes:[
+            'travelling',
+            'riding'
+        ]
+    });
+});
+app.get('/home', (req, res)=>{
+    res.render('home.hbs',{
+        welcomeMessage:'welcome to my page',
+        pageTitle:'Home Page',
+
+    });
+});
+
+
+app.get('/about', (req, res)=>{
+    res.render('about.hbs',{
+        pageTitle:'About Page',
+    });
+});
+
+app.get('/bad', (req, res)=>{
+    res.send({
+        errormessage: "Unable to connect to the server"
+    });
+});
+
+app.listen(3000, ()=>{
+    console.log("Server is up on port 3000");
+});
